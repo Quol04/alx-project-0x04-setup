011ff013c8c5f0036222b8a93ce8cc2bce30eb46
quol <kuolamol77@gmail.com>
2025-10-01 12:06:38 +0300

    modified folder and files

---
+++ alx-project-0x04/components/common/Button.tsx
@@ -0,0 +1,31 @@
+
+import { ButtonProps } from "../../interface";
+
+const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
+
+  const getButtonClasses = () => {
+    const baseClasses = "px-6 py-2 text-sm font-semibold rounded-lg transition duration-300 text-white";
+    
+    switch (buttonBackgroundColor) {
+      case 'red':
+        return `${baseClasses} bg-red-500 hover:bg-red-400`;
+      case 'blue':
+        return `${baseClasses} bg-blue-500 hover:bg-blue-400`;
+      case 'orange':
+        return `${baseClasses} bg-orange-500 hover:bg-orange-400`;
+      case 'green':
+        return `${baseClasses} bg-green-500 hover:bg-green-400`;
+      default:
+        return `${baseClasses} bg-slate-500 hover:bg-slate-400`;
+    }
+  }
+
+
+  return (
+    <button onClick={action} className={`${getButtonClasses()} ${buttonSize || ''}`}>
+      {buttonLabel}
+    </button>
+  )
+}
+
+export default Button;
